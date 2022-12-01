a9e56b8f6751886bca4eff6e633ad7ebfe1124da
Jaayden Halko <jaayden.halko@gmail.com>
2022-12-01 18:39:17 -0500

    fix

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -59,7 +59,7 @@ const Header = (props) => {
         <img alt="logo" src={logo} />
       </Link>
 
-      {this.props.currentUser ? (
+      {props.currentUser ? (
         <LoggedInView currentUser={props.currentUser} />
       ) : (
         <LoggedOutView currentUser={props.currentUser} />
