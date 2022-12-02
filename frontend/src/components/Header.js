caba0111ba54e7934de005038740c079e4a74994
Jaayden Halko <jaayden.halko@gmail.com>
2022-12-01 20:36:23 -0500

    fix header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const LoggedInView = (props) => {
   );
 };
 
-const Header = () => {
+const Header = (props) => {
   return (
     <nav
       className="navbar navbar-expand-md navbar-dark"
@@ -59,10 +59,10 @@ const Header = () => {
         <img alt="logo" src={logo} />
       </Link>
 
-      {this.props.currentUser ? (
-        <LoggedInView currentUser={this.props.currentUser} />
+      {props.currentUser ? (
+        <LoggedInView currentUser={props.currentUser} />
       ) : (
-        <LoggedOutView currentUser={this.props.currentUser} />
+        <LoggedOutView currentUser={props.currentUser} />
       )}
     </nav>
   );
